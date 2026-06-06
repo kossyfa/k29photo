b32daca3e3ca486da45f1196a9be4bcb7b540668
kossyfa <theodorakossyfa@gmail.com>
2026-06-07 01:30:16 +0300

    Update schema_er.sql

--- schema_er.sql
+++ schema_er.sql
@@ -1,3 +1,9 @@
+-- =====================================================
+-- Χρησιμοποιήθηκε για την παραγωγή του E/R διαγράμματος 
+-- μέσω του εργαλείου MySQL Workbench (Reverse Engineer)
+-- =====================================================
+
+
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
